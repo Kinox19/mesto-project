1f025ed8d0d0110e6218e7e6a6ec6d6cce0e14a8
Kinox19 <puchkouartem@gmail.com>
2023-05-04 12:11:16 +0300

    Добавлен вывод карточек через js

---
+++ vendor/script.js
@@ -0,0 +1,48 @@
+const initialCards = [
+        {
+            name: 'Архыз',
+            link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        },
+        {
+            name: 'Челябинская область',
+            link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        },
+        {
+            name: 'Иваново',
+            link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        },
+        {
+            name: 'Камчатка',
+            link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        },
+        {
+            name: 'Холмогорский район',
+            link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        },
+        {
+            name: 'Байкал',
+            link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        }
+    ];
+
+const cardsGrid = document.querySelector('.cards-grid');
+const userProfilePopUp = document.querySelector('.popup');
+
+function showInitialCards(titleText, imageLink){
+    const cardTemplate = document.querySelector('#card').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = titleText;
+    cardElement.querySelector('.card__image').src = imageLink;
+    cardsGrid.append(cardElement);
+}
+
+function renderCards(card) {
+    const cardTitle = card.name;
+    const cardImage = card.link;
+    showInitialCards(cardTitle, cardImage);
+  }
+
+initialCards.map(renderCards);
+
+
+
