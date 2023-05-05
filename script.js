7761f517c3d0e74af759c311e191b6eba225a850
Kinox19 <puchkouartem@gmail.com>
2023-05-05 16:33:54 +0300

    fix: вынесен массив

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
-
 const cardsGrid = document.querySelector('.cards-grid');
 const cardTemplate = document.querySelector('#card').content;
 
@@ -66,29 +38,7 @@ closePopUpButtons.forEach(button => {
     });
 });
 
-function showInitialCards(titleText, imageLink){
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__title').textContent = titleText;
-    cardElement.querySelector('.card__image').src = imageLink;
-    cardElement.querySelector('.card__image').alt = titleText;
 
-
-
-    cardElement.querySelector('.card__image').addEventListener('click', openImageAsPopUp);
-    // лайкаем карточку
-    cardElement.querySelector('.button_type_like').addEventListener('click', function (e) {
-        e.target.classList.toggle('button_type_like_active');
-      });
-
-    // удаляем карточку
-    cardElement.querySelector('.button_type_delete').addEventListener('click', function (e) {
-        e.target.closest('.card').remove();
-    });
-
-
-
-    cardsGrid.append(cardElement);
-}
 
 function renderCards(card) {
     const cardTitle = card.name;
@@ -142,6 +92,30 @@ function addNewCard () {
     newCard.querySelector('.card__title').textContent = placeNameInput.value;
 
     return newCard
+}
+
+function showInitialCards(titleText, imageLink){
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = titleText;
+    cardElement.querySelector('.card__image').src = imageLink;
+    cardElement.querySelector('.card__image').alt = titleText;
+
+
+
+    cardElement.querySelector('.card__image').addEventListener('click', openImageAsPopUp);
+    // лайкаем карточку
+    cardElement.querySelector('.button_type_like').addEventListener('click', function (e) {
+        e.target.classList.toggle('button_type_like_active');
+      });
+
+    // удаляем карточку
+    cardElement.querySelector('.button_type_delete').addEventListener('click', function (e) {
+        e.target.closest('.card').remove();
+    });
+
+
+
+    cardsGrid.append(cardElement);
 }
 
 function openImageAsPopUp(e){
